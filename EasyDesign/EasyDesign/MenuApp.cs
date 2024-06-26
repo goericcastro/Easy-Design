43a2526780db12c4586e6461fe77ef3c21b6db60
Eric Castro <eric.castro.ingenieria@gmail.com>
2024-06-26 04:18:09 -0300

    1st update
    Update the file for first plugin

---
+++ EasyDesign/EasyDesign/MenuApp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using Autodesk.Revit.UI;
+
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+
+namespace EasyDesign
+{
+    public class MenuApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = RibbonPanel(application);
+
+
+            return Result.Succeeded;
+        }
+
+        public RibbonPanel RibbonPanel(UIControlledApplication application)
+        {
+            string tab = "EasyDesign";
+            RibbonPanel ribbonPanel = null;
+            try
+            {
+                application.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                application.CreateRibbonPanel(tab, "ARCHITECTURE");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            List<RibbonPanel> panels = application.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "Arch"))
+            {
+                ribbonPanel = p;
+            }
+            return ribbonPanel;
+        }
+    }
+}
--- EasyDesign/EasyDesign/Properties/AssemblyInfo.cs
+++ EasyDesign/EasyDesign/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Easy-Design")]
+[assembly: AssemblyTitle("EasyDesign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Easy-Design")]
+[assembly: AssemblyProduct("EasyDesign")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2d74690b-004e-4e86-afb9-45f3b50648c9")]
+[assembly: Guid("cb2ac0ad-efb2-43e9-80b9-d174c6918bd6")]
 
 // Version information for an assembly consists of the following four values:
 //
